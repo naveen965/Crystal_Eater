f214a6f9ae05cafce524451eee36130f90af77cf
naveen965 <39030430+naveen965@users.noreply.github.com>
2021-01-20 19:53:24 +0530

    Edit GameScene and Add new Scene

--- Crystal Eater/Assets/Scripts/ButtonsEvents.cs
+++ Crystal Eater/Assets/Scripts/ButtonsEvents.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameHandler : MonoBehaviour
+public class ButtonsEvents : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameHandler.Start");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
 }
---
+++ Crystal Eater/Assets/Scripts/SnakePart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakePart : MonoBehaviour
+{
+    Snake snake;
+    Field field;
+
+    void Start()
+    {
+        snake = transform.parent.GetComponent<Snake>();
+        field = FindObjectOfType<Field>();
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("food"))
+        {
+            var lastPart = snake.bodyParts[snake.bodyParts.Count - 1];
+
+            var newPart = Instantiate(lastPart.gameObject, lastPart.position - lastPart.up * snake.step, lastPart.rotation, transform.parent);
+            snake.bodyParts.Add(newPart.transform);
+
+            if (snake.stepTime > snake.minStepTime)
+                snake.stepTime -= snake.stepTimeAdder;
+        }
+        else
+        {
+            snake.Die();
+            return;
+        }
+
+        Destroy(coll.gameObject);
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x) > field.width / 2f * field.cellSize + 1f ||
+            Mathf.Abs(transform.position.y) > field.height / 2f * field.cellSize)
+            snake.Die();
+    }
+}
