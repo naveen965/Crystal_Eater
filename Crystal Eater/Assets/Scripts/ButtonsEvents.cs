e4d5ad7c087385c1faab48c4aae164cd68692afd
naveen965 <39030430+naveen965@users.noreply.github.com>
2021-01-21 20:01:23 +0530

    Modified

--- Crystal Eater/Assets/Scripts/ButtonsEvents.cs
+++ Crystal Eater/Assets/Scripts/ButtonsEvents.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsEvents : MonoBehaviour
 {
@@ -22,4 +23,9 @@ public class ButtonsEvents : MonoBehaviour
         Debug.Log("Quit");
     }
 
+    public void NextScene()
+    {
+        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
