cabb320e3f110c6e1e02b7be2b9995ec5f16b12a
naveen965 <39030430+naveen965@users.noreply.github.com>
2021-01-25 12:24:59 +0530

    Edited

---
+++ Crystal Eater/Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject snakePrefab;
+    public Snake head;
+    public Snake tail;
+    public int NESW;
+    public Vector2 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("TimerInvoke", 0, .5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TimerInvoke()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        GameObject temp;
+        nextPos = head.transform.position;
+
+        switch (NESW)
+        {
+            case 0:
+                nextPos = new Vector2(nextPos.x, nextPos.y + 1);
+                break;
+            case 1:
+                nextPos = new Vector2(nextPos.x + 1, nextPos.y);
+                break;
+            case 2:
+                nextPos = new Vector2(nextPos.x, nextPos.y - 1);
+                break;
+            case 3:
+                nextPos = new Vector2(nextPos.x - 1, nextPos.y);
+                break;
+        }
+        temp = (GameObject)Instantiate(snakePrefab, nextPos, transform.rotation);
+        head.SetNext(temp.GetComponent<Snake>());
+        head = temp.GetComponent<Snake>();
+
+        return;
+    }
+}
--- Crystal Eater/Assets/Scripts/Snake.cs
+++ Crystal Eater/Assets/Scripts/Snake.cs
@@ -1,88 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Snake : MonoBehaviour
 {
-    public List<Transform> bodyParts = new List<Transform>();
+    public Snake next;
 
-    public float minStepTime;
-    public float maxStepTime;
-
-    public float stepTimeAdder;
-
-
-    [HideInInspector]
-    public float stepTime;
-    [HideInInspector]
-    public float step;
-    [HideInInspector]
-    public float delta = 0f;
-
-    Transform head;
-    Vector2 moveDirection = Vector2.up;
-
-    // Start is called before the first frame update
-    void Start()
+    public void SetNext(Snake IN)
     {
-        head = bodyParts[0];
-        stepTime = maxStepTime;
+        next = IN;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Snake GetNext()
     {
-        Control();
-        if (delta >= stepTime)
-        {
-            Move();
-            delta = 0f;
-        }
-        delta += Time.deltaTime;
+        return next;
     }
 
-    void Control()
+    public void RemoveTail()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            moveDirection = Vector2.up;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            moveDirection = Vector2.down;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveDirection = Vector2.left;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            moveDirection = Vector2.right;
-        }
-
-        if (moveDirection == -(Vector2)head.up)
-        {
-            moveDirection = head.up;
-        }
-    }
-
-    void Move()
-    {
-        for (int i = bodyParts.Count - 1; i >= 1; i--)
-        {
-            var part = bodyParts[i];
-            var nextPart = bodyParts[i - 1];
-            part.rotation = nextPart.rotation;
-            part.position = nextPart.position;
-        }
-
-        head.up = moveDirection;
-        head.position += head.up * step;
-    }
-
-    public void Die()
-    {
-        SceneManager.LoadScene(0);
+        Destroy(this.gameObject);
     }
 }
